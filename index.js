5937ca5319c82f9b374fe0be5f984b2b9fa6964f
Augusto Brito <britoaugusto1@gmail.com>
2024-10-02 23:17:21 -0300

    se agrego una ruta de prueba para mandar una respuesta

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ app.set('port', process.env.PORT || 4000)
 app.listen(app.get('port'), ()=>{
     console.info(`Estoy escuchando el puerto `+ app.get('port'))
 })
+
+app.get('/prueba', (req, res)=>{
+console.log('desde la funcion de prueba')
+res.send('Desde el backend del proyecto crudCocina')
+})
