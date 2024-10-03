66342c272b16167ccf16ce844a09396ced232923
Augusto Brito <britoaugusto1@gmail.com>
2024-10-02 21:21:24 -0300

    agregamos el puerto 4000 o una variable de entorno

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('Hola mundo 2')
+import express from "express";
+
+const  app = express();
+app.set('port', process.env.PORT || 4000)
+app.listen(app.get('port'), ()=>{
+    console.info(`Estoy escuchando el puerto `+ app.get('port'))
+})
