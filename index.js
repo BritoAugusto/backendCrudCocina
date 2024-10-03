182a7defb405a6bd58b8fae4924e114a83bf0d02
Augusto Brito <britoaugusto1@gmail.com>
2024-10-03 13:13:14 -0300

    agregamos los middlewares cors, morgan y los de express

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import express from "express";
+import cors from 'cors';
+import morgan from "morgan";
+
 
 const  app = express();
 app.set('port', process.env.PORT || 4000)
@@ -6,7 +9,11 @@ app.listen(app.get('port'), ()=>{
     console.info(`Estoy escuchando el puerto `+ app.get('port'))
 })
 
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
 app.get('/prueba', (req, res)=>{
-console.log('desde la funcion de prueba')
 res.send('Desde el backend del proyecto crudCocina')
 })
