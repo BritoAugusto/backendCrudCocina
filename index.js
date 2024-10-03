590a9290725e8dfdf0e2a7b3afc039491cd872a3
Augusto Brito <britoaugusto1@gmail.com>
2024-10-03 13:24:34 -0300

    agregamos los middlewares para configurar un archivo estatico

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import cors from 'cors';
 import morgan from "morgan";
-
+import path, { dirname } from 'path'
+import { fileURLToPath } from "url";
+import exp from "constants";
 
 const  app = express();
 app.set('port', process.env.PORT || 4000)
@@ -13,6 +15,10 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+app.use(express.static(path.join(__dirname, '/public')));
+
 
 app.get('/prueba', (req, res)=>{
 res.send('Desde el backend del proyecto crudCocina')
